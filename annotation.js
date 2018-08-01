90f311bb99218cf80319b2a806789599eadb4413
gxtgong <40364552+gxtgong@users.noreply.github.com>
2018-08-01 15:56:58 -0400

    Add server written in node.js

--- annotation.js
+++ annotation.js
@@ -4,70 +4,249 @@ var annotation = {};
 
 var vidDOM = document.getElementById('vid');
 
-var videosrc;
+var videosrc = null;
 
+var intervals = [];
 
+var tags = {};
+
+var booPL = false;
+
+var plStart = null;
+
+var emotion = null;
+
+var complete = false;
 
 $.getJSON('video.json', function(data){
-	console.log("video.json Read");
-	videoNames = data;
-	for (let vn of videoNames.nameArray) {
-		//console.log(vn);
-		if (vn in annotation) {
-			if (annotation.vn.complete) {
-				$('#list-video').append('<a href="#" class="list-group-item list-group-item-action list-group-item-success">'+vn+'</a>');
-			}else{
-				$('#list-video').append('<a href="#" class="list-group-item list-group-item-action list-group-item-warning">'+vn+'</a>');
-			}
-		}else{
-			$('#list-video').append('<a href="#" class="list-group-item list-group-item-action">'+vn+'</a>');
-		}
-	}
-	$("a").click(function(){
-		videosrc = $(this).html();
-		console.log(videosrc);
-		$("video").attr('src', 'videodataset/'+videosrc);
-		vid.addEventListener('loadedmetadata', function(){
-			console.log(vid.duration);
-		})
-	});
+
+    console.log("video.json Read");
+    videoNames = data;
+    for (let vn of videoNames.nameArray) {
+        $('#list-video').append('<a href="#" class="list-group-item list-group-item-action" id="'+vn+'" >'+vn+'</a>');
+    }
+    applyAnnotationOf("candice");
+    annotation["annotator"] = "candice";
+    // load video 
+    $("a").click(function(){
+        //save last info
+        if (videosrc != null) {
+            annotation[videosrc] = {};
+            annotation[videosrc]["intervals"] = intervals;
+            annotation[videosrc]["tags"] = tags;
+            annotation[videosrc]["complete"] = complete;
+            
+
+        }
+
+        //load video src
+        videosrc = $(this).html();
+        console.log(videosrc);
+        $("video").attr('src', 'videodataset/'+videosrc);
+
+        //after video is loaded, add eventlistener
+        vidDOM.addEventListener('loadedmetadata', function(){
+            //console duration
+            console.log(vidDOM.duration);
+            //retrieve previous annoatation
+            if (videosrc in annotation) {
+                intervals = annotation[videosrc]["intervals"];
+                tags = annotation[videosrc]["tags"];
+                emotion = annotation[videosrc]["emotion"];
+            }else{
+                console.log("new video");
+                intervals = [[0,vidDOM.duration]];
+                tags = {};
+                emotion = [];
+            }
+            document.getElementById("test-pl").innerHTML = intervals.toString();
+        });
+
+        //turn off pl button
+        booPL = false;
+        document.getElementById("btn-pl").classList.remove("btn-warning");
+        document.getElementById("btn-pl").classList.add("btn-success");
+        document.getElementById("btn-pl").innerHTML = "Punchline Off";
+        plStart = null;
+
+        //apply changes to annotation interface
+        applyTags();
+        if (emotion == null || emotion[0] == null) {
+            $("#test-emo").html("Emotion: ");
+        }else{
+            $("#test-emo").html("Emotion: ("+emotion[0]+" "+emotion[1]+")");
+        }
+
+        document.getElementById("div-vid-ano").classList.remove("d-none");
+    });
 });
 
+function applyTags() {
+    $(".list-group button").each(function(){
+        tag = this.innerHTML;
+        if (tags[tag]) {
+            this.classList.add("active");
+        }else{
+            this.classList.remove("active");
+        }
+    })
+}
+
+function applyAnnotationOf(name) { // apply annotation to the list of names (not a particular video)
+    $.getJSON(name+'.json', function(data){
+        console.log(name+'.json Read');
+        console.log(JSON.stringify(data));
+        annotation = data;
+        for (var vn in data) {
+            if (vn !== "annotator") {
+                if (data[vn]["complete"]) {
+                    document.getElementById(vn).classList.add("list-group-item-success");
+                } else {
+                    document.getElementById(vn).classList.add("list-group-item-warning");
+                }
+            }
+        }
+    });
+}
+
+
 $(document).ready(function(){
-	$(".list-group button").click(function(){
-		var cl = this.classList;
-		if (cl.contains("active")) {
-			cl.remove("active");
-		}else{
-			cl.add("active");
-		}
-	});
-	$("img").click(function(e){
+    // append tag functions
+    $(".list-group button").click(function(){
+        var tag = this.innerHTML;
+        if (tags[tag]) {
+            tags[tag] = false;
+        }else{
+            tags[tag] = true;
+        }
+        applyTags();
+    });
+
+    // punchline button
+    $("#btn-pl").click(function(){
+        //get vid time
+        var t = vidDOM.currentTime;
+        //change button appearance
+        var cl = this.classList;
+        // turn on
+        if (!booPL) {
+            booPL = true;
+            plStart = t;
+            cl.remove("btn-success");
+            cl.add("btn-warning");
+            this.innerHTML = "Punchline On";
+        }else{
+            booPL = false;
+            cl.remove("btn-warning");
+            cl.add("btn-success");
+            this.innerHTML = "Punchline Off";
+            plStart = null;
+        }
+    });
+    $("#btn-clear").click(function(){
+        intervals = [[0,vidDOM.duration]];
+        intervalsToBars();
+    })
+
+    // img for emo
+    $("img").click(function(e){
         var xPos = e.pageX - $("img").offset().left;
         var yPos = e.pageY - $("img").offset().top;
         $("#test-emo").html("Emotion: ("+xPos+" "+yPos+")");
+        emotion = [xPos, yPos];
         console.log("Emotion: ("+xPos+" "+yPos+")");
     });
-	$("video").on("timeupdate", function(event){
-		var wid = parseInt(this.currentTime/this.duration*100) + "\%";
-		document.getElementById("pbar-played").style.width = wid;
-		$("#test").html(this.currentTime+" width: "+wid);
-	});
-});
+    // playing
+    $("video").on("timeupdate", function(event){
+        //var wid = parseInt(this.currentTime/this.duration*100) + "\%";
+        //document.getElementById("pbar-played").style.width = wid;
+        //$("#test").html(this.currentTime+" width: "+wid);
+        updateIntervals(plStart, this.currentTime);
+        intervalsToBars();
+        $("#test-pl").html(intervals.toString());
+    });
 
+    $("#file-save").click(function(e){
+        $.post(annotation.annotator+'.json', JSON.stringify(annotation, null, 4), function(data, status){
+            console.log("Data: "+ data + " Status: " + status);
+        });
+    })
+});
 
 var annoName = $('input[name="annotator"]:checked').val();
 console.log("annotator"+annoName);
 
-
-
-
-/*$.ajax({
-    method : "POST",
-    url : "save_to_json.php",
-    data : {
-        json : JSON.stringify(annotation)
+function updateIntervals(start, end){
+    if (booPL) {
+        var i = 0;
+        var j = 0;
+        var prev = 0;
+        var aft = 0;
+        while (i < intervals.length) {
+            [a, b] = intervals[i];
+            if ((start <= b && start >= a)||(start < a && start > prev)) {
+                j = i;
+                if (start >= a){
+                    prev = a;
+                }else{
+                    aft = prev;
+                    prev = null;
+                }
+                while (j < intervals.length) {
+                    [c, d] = intervals[j];
+                    if ((end <= d && end >= c)||(end < c && end > aft)) {
+                        if (end >= c) {
+                            aft = d;
+                        }else{
+                            aft = null;
+                        }
+                        break;
+                    }else{
+                        j ++;
+                        aft = d;
+                    }
+                }
+                break;
+            }else{
+                i ++;
+                prev = b;
+            }
+        }
+        if (prev==null){
+            if (aft==null){
+                intervals.splice(i, j-i);
+            }else{
+                intervals.splice(i, j-i+1, [end, aft]);
+            }
+        }else{
+            if (aft==null){
+                intervals.splice(i, j-i, [prev, start]);
+            }else{
+                intervals.splice(i, j-i+1, [prev, start], [end, aft]);
+            }
+        }
     }
-});*/
+}
 
-//$.post('save_to_json.php', {json: JSON.stringify(annotation)});
+function intervalsToBars(){
+    var bars = [];
+    var d = vidDOM.duration;
+    var last;
+    var result = "";
+    for (var index in intervals) {
+        [a,b] = [parseInt(intervals[index][0]/d*100), parseInt(intervals[index][1]/d*100)];
+        bars[index] = [a,b]
+        if (index > 0) {
+            result += getBarHTML(a-last, "bg-warning")
+        }
+        result += getBarHTML(b-a, "bg-success");
+        last = b;
+    }
+    $(".progress").html(result);
+}
+
+function getBarHTML(percent, barType){
+    //bg-success for green/off
+    //bg-warning for yellow/on
+    return '<div class="progress-bar '+ barType +'" role="progressbar" style="width: '+percent+'%" id="pbar-played"></div>'
+}
---
+++ server.js
@@ -0,0 +1,108 @@
+var http = require("http");
+var fs = require("fs");
+var path = require("path");
+
+var videoNames = require("./video");
+
+var video = {};
+
+function processed (s) {
+	return s.replace(/%20/g, " ");
+}
+
+video["nameArray"] = fs.readdirSync("./videodataset");
+fs.writeFileSync('video.json', JSON.stringify(video, null, 4));
+
+http.createServer(function(req, res) {
+
+    console.log(`${req.method} request for ${req.url}`);
+
+    if (req.method === "GET") {
+        if (req.url === "/") {
+            fs.readFile("./annotation.html", "UTF-8", function(err, html) {
+                res.writeHead(200, {
+                    "Content-Type": "text/html"
+                });
+                res.end(html);
+            });
+
+        } else if (req.url.match(/.js$/)) {
+
+            var jsPath = path.join(__dirname, req.url);
+            var fileStream = fs.createReadStream(jsPath, "UTF-8");
+
+            res.writeHead(200, {
+                "Content-Type": "text/js"
+            });
+
+            fileStream.pipe(res);
+
+        } else if (req.url.match(/.png$/)) {
+
+            var imgPath = path.join(__dirname, req.url);
+            var imgStream = fs.createReadStream(imgPath);
+
+            res.writeHead(200, {
+                "Content-Type": "image/png"
+            });
+
+            imgStream.pipe(res);
+
+        } else if (req.url.match(/.json$/)) {
+
+            var jsonPath = path.join(__dirname, req.url);
+            var jsonStream = fs.createReadStream(jsonPath, "UTF-8");
+
+            res.writeHead(200, {
+                "Content-Type": "text/json"
+            });
+
+            jsonStream.pipe(res);
+
+        } else if (req.url.match(/.mp4$/)) {
+
+            var mp4Path = path.join(__dirname, processed(req.url));
+            var mp4Stream = fs.createReadStream(mp4Path);
+
+            res.writeHead(200, {
+            	"Content-Type" : "video/mp4"
+            });
+
+            mp4Stream.pipe(res);
+
+        } else if (req.url.match(/.webm$/)) {
+
+            var webmPath = path.join(__dirname, processed(req.url));
+            var webmStream = fs.createReadStream(webmPath);
+
+            res.writeHead(200, {
+            	"Content-Type" : "video/webm"
+            });
+
+            webmStream.pipe(res);
+
+        } else {
+
+            res.writeHead(404, {
+                "Content-Type": "text/plain"
+            });
+            res.end("404 File Not Found");
+        }
+    } else if (req.method === "POST") {
+    	var body = "";
+    	req.on("data", function(chunk){
+    		body += chunk;
+    	})
+    	req.on("end", function() {
+            name = JSON.parse(body).annotator;
+            fs.writeFileSync(name+".json", body);
+            fs.writeFile(name+".json", body, function(){
+                console.log("WRITE annotation" + " TO " + name + ".json");
+            });
+    	})
+    }
+
+
+}).listen(3000);
+
+console.log("File server running on port 3000");
